ec8475c38265cebcdaac158a2cd4c8e9ad337b01
Marok2019 <mauken2009@gmail.com>
2024-12-09 18:07:08 -0300

    fix auth

--- appespacioadmin/src/components/Auth.jsx
+++ appespacioadmin/src/components/Auth.jsx
@@ -1,103 +1,135 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from '../api/axios'; // Asegúrate de que este archivo esté correctamente configurado
+import axios from '../api/axios'; // Asegúrate de que este archivo está correctamente configurado
 
 const Auth = () => {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        document.getElementById('current-year').textContent = new Date().getFullYear();
-    }, []);
+  // Mostrar el año actual en el pie de página
+  useEffect(() => {
+    document.getElementById('current-year').textContent = new Date().getFullYear();
+  }, []);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            // Llama al backend con las credenciales
-            const response = await axios.post('/api/users/login', { email, password });
-            
-            // Guarda el token en el almacenamiento local o sesión
-            localStorage.setItem('token', response.data.token);
-            
-            // Redirige al usuario dependiendo de su rol (ejemplo)
-            if (response.data.role === 'resident') {
-                navigate('/residente-main');
-            } else if (response.data.role === 'admincondo') {
-                navigate('/admin-condominio');
-            } else if (response.data.role === 'conserje') {
-                navigate('/conserje-main');
-            } else if (response.data.role === 'superadmin') {
-                navigate('/superadmin-main');
-            } else if (response.data.role === 'directive') {
-                navigate('/directiva');
-            } else {
-                console.error('Rol no reconocido:', response.data.role);
-            }
-            
-        } catch (err) {
-            setError('Credenciales inválidas o error en el servidor.');
-        }
-    };
+  // Navegar según el rol en función de la respuesta
+  const navigateToRole = (role) => {
+    switch (role) {
+      case 'resident':
+        navigate('/residente-main');
+        break;
+      case 'admincondo':
+        navigate('/admin-condominio');
+        break;
+      case 'conserje':
+        navigate('/conserje-main');
+        break;
+      case 'superadmin':
+        navigate('/superadmin-main');
+        break;
+      case 'directive':
+        navigate('/directiva');
+        break;
+      default:
+        alert('Rol no reconocido');
+        navigate('/auth');
+    }
+  };
 
-    return (
-        <div className="bg-dark">
-            {/* Header */}
-            <div className="header-container d-flex align-items-center">
-                <img src="https://i.ibb.co/FW5SBG3/logo-no-background.png" alt="Logo" className="header-logo" />
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true); // Estado para mostrar cargando mientras se envía la solicitud
+    setError('');
+
+    try {
+      // Realizar solicitud con credenciales
+      const response = await axios.post('/api/users/login', { email, password });
+
+      // Validar la respuesta y guardar token y rol
+      if (response?.data?.token && response?.data?.role) {
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('role', response.data.role);
+
+        navigateToRole(response.data.role);
+      } else {
+        throw new Error('Datos inválidos');
+      }
+    } catch (err) {
+      setError('Credenciales inválidas o error en el servidor.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="bg-dark">
+      {/* Header */}
+      <div className="header-container d-flex align-items-center">
+        <img
+          src="https://i.ibb.co/FW5SBG3/logo-no-background.png"
+          alt="Logo"
+          className="header-logo"
+        />
+      </div>
+
+      {/* Body */}
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            {/* Card con formulario de inicio de sesión */}
+            <div className="card">
+              <div className="card-header text-center">EspacioAdmin</div>
+              <div className="card-body">
+                {/* Formulario */}
+                <form id="loginForm" onSubmit={handleSubmit}>
+                  {/* Mostrar error si existe */}
+                  {error && <p className="text-danger">{error}</p>}
+                  <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Correo Electrónico:</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Contraseña:</label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className={`btn ${loading ? 'btn-secondary' : 'btn-primary'} w-100`}
+                    disabled={loading}
+                  >
+                    {loading ? 'Cargando...' : 'Iniciar Sesión'}
+                  </button>
+                </form>
+              </div>
             </div>
-
-            {/* Body */}
-            <div className="container mt-5">
-                <div className="row justify-content-center">
-                    <div className="col-md-4">
-                        {/* Card */}
-                        <div className="card">
-                            <div className="card-header">EspacioAdmin</div>
-                            <div className="card-body">
-                                {/* Formulario */}
-                                <form id="loginForm" onSubmit={handleSubmit}>
-                                    {/* Mostrar error si existe */}
-                                    {error && <p className="text-danger">{error}</p>}
-                                    <div className="mb-3">
-                                        <label htmlFor="email" className="form-label">Correo:</label>
-                                        <input
-                                            type="email"
-                                            className="form-control"
-                                            id="email"
-                                            value={email}
-                                            onChange={(e) => setEmail(e.target.value)}
-                                            required
-                                        />
-                                    </div>
-                                    <div className="mb-3">
-                                        <label htmlFor="password" className="form-label">Contraseña:</label>
-                                        <input
-                                            type="password"
-                                            className="form-control"
-                                            id="password"
-                                            value={password}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                            required
-                                        />
-                                    </div>
-                                    <button type="submit" className="btn btn-primary">Iniciar sesión</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            {/* Footer */}
-            <footer className="bg-dark py-3 mt-5">
-                <div className="container">
-                    <p className="text-center">&copy; <span id="current-year"></span> Todos los derechos reservados</p>
-                </div>
-            </footer>
+          </div>
         </div>
-    );
+      </div>
+
+      {/* Footer */}
+      <footer className="bg-dark py-3 mt-5">
+        <div className="container">
+          <p className="text-center">&copy; <span id="current-year"></span> Todos los derechos reservados</p>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Auth;
